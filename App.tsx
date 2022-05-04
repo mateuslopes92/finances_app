37c1b2ba79375f825d62ac1007bbe6e36294d424
Mateus Lopes <mateuslopes92@gmail.com>
2022-05-04 16:37:25 -0300

    Project created and added typescript

---
+++ App.tsx
@@ -0,0 +1,6 @@
+import { Dashboard } from './src/screens/Dashboard';
+import React from 'react';
+
+export default function App() {
+  return <Dashboard />
+}
---
+++ src/screens/Dashboard.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+import React from 'react';
+
+export function Dashboard(){
+  return (
+    <View>
+      <Text>Dashboard</Text>
+    </View>
+  )
+}
